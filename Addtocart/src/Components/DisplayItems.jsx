8241e134f751a1cf0e95b5db856ab56664b48751
uniqueneu3900 <62101934+unique3900@users.noreply.github.com>
2023-04-02 18:48:33 +0545

    complete display css

--- Addtocart/src/Components/DisplayItems.jsx
+++ Addtocart/src/Components/DisplayItems.jsx
@@ -7,15 +7,25 @@ import { useDispatch } from 'react-redux';
 const DisplayItems = () => {
     const dispatch = useDispatch();
   return (
-    <div className='container'>
-          <div className="itemContainer">
-              <div className="itemBox">
-                  <img src="https://i.dummyjson.com/data/products/1/thumbnail.jpg" alt="" />
-                  <h2>Iphone</h2>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, voluptatem laboriosam. Quaerat nulla odit, eaque nam nisi error molestiae voluptate.</p>
-                  <h3>12000</h3>
-                  <button onClick={()=>{dispatch(addToCart())}}>Add To Cart</button>
-              </div>
+      <div className='container wrapper'>
+             
+          <div className="itemContainer scrollbar" id="style-4">
+              
+              {
+                  ProductData.map((item, index) => {
+                      return (
+                        <div className="itemBox" key={index}>
+                        <img src="https://i.dummyjson.com/data/products/1/thumbnail.jpg" alt="" />
+                        <h2>Iphone</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, voluptatem laboriosam. Quaerat nulla odit, eaque nam nisi error molestiae voluptate.</p>
+                        <h3>12000</h3>
+                        <button onClick={()=>{dispatch(addToCart())}}>Add To Cart</button>
+                    </div>
+                      )
+                    
+                  })
+              }
+
       </div>
     </div>
   )
